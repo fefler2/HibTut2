e561097d3a413b65e5d3254ced3bcbe38ad43a20
giciarz412 <tomaszfefler1990@gmail.com>
2018-05-09 18:18:01 +0200

    zmiana 1

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,8 +1,25 @@
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "banks_table")
+
 public class Bank {
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(generator = "incrementator")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
 
 
 }
